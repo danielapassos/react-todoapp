24bf1df7cbedf238e6486c47268d12e98c0fb613
Daniela Passos <moitidani@gmail.com>
2023-06-05 14:51:26 -0500

    Initial commit

---
+++ components/Header/index.tsx
@@ -0,0 +1,12 @@
+import { Image, View } from "react-native";
+
+
+import { styles } from "./styles";
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <Text>To-do</Text>
+    </View>
+  )
+}
